6973e9a58a473213260dcfe59fbf68e4e1834b1c
Tedtya Rady <tedtyarady@gmail.com>
2022-05-27 20:28:44 +1000

    headers_full and nav_some

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import './header.css';
 import CTA from "./CTA";
+import ME from '../../assets/me.png';
+import HeaderSocial from "./HeaderSocial";
 
 const Header = () => {
     return(
@@ -10,6 +12,14 @@ const Header = () => {
                 <h1>Tedtya RADY</h1>
                 <h5 className="text-light">FullStack Developer</h5>  
                 <CTA/>
+
+                <HeaderSocial/>
+
+                <div className="me">
+                    <img src={ME} alt='me'></img>
+                </div>
+
+                <a href="#contact" className="scroll__down" >Scroll Down</a>
             </div>
         </header> 
     )
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import './nav.css';
+import {BiHome} from 'react-icons/bi';
+import {BiUser} from 'react-icons/bi';
+import {GiTrophyCup} from 'react-icons/gi';
+import {RiHandHeartLine} from 'react-icons/ri';
+import {AiOutlinePhone} from 'react-icons/ai';
+
 
 const Nav = () => {
     return(
-        <div>Nav</div>
-    )
+        <nav>
+            <a href="#"><BiHome/></a>
+            <a href="#about"><BiUser/></a>
+            <a href="#experience"><GiTrophyCup/></a> 
+            <a href="#services"><RiHandHeartLine/></a>
+            <a href="#contact"><AiOutlinePhone/></a>
+        </nav>
+    ) 
 }
 
-export default Nav
+export default Nav  
