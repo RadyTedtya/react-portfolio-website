ac3f534f76e2c59d0ce8b6bad8386824b429ff00
Tedtya Rady <tedtyarady@gmail.com>
2022-05-28 00:53:05 +1000

    update nav. Almost done

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React from 'react';
 import './nav.css';
 import {BiHome} from 'react-icons/bi';
 import {BiUser} from 'react-icons/bi';
 import {GiTrophyCup} from 'react-icons/gi';
 import {RiHandHeartLine} from 'react-icons/ri';
 import {AiOutlinePhone} from 'react-icons/ai';
+import {useState} from 'react';
 
 
 const Nav = () => {
+    const [activeNav,setActiveNav] = useState('#')
     return(
         <nav>
-            <a href="#"><BiHome/></a>
+            <a href="#home" className={activeNav === '#' ? 'active' : ''}><BiHome/></a>
             <a href="#about"><BiUser/></a>
             <a href="#experience"><GiTrophyCup/></a> 
             <a href="#services"><RiHandHeartLine/></a>
